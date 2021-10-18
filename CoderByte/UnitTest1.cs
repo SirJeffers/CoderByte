870feea85297c25ffc74c440c9238ebcc402d00f
Jeffrey Belford <belfordjeffrey@gmail.com>
2021-10-18 15:48:49 -0400

    base commit

--- CoderByte/UnitTest1.cs
+++ CoderByte/UnitTest1.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
 
 namespace CoderByte
 {
     public class Tests
     {
+        IWebDriver driver;
+        string Path;
         [SetUp]
         public void Setup()
         {
+            Path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            driver = new ChromeDriver(Path+@"\Drivers\");
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("http://www.crawco.co.uk");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
