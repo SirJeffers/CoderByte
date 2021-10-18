933ec74a50ddbb4b576e1fd927e3e9461eb2d252
Jeffrey Belford <belfordjeffrey@gmail.com>
2021-10-18 18:21:56 -0400

    final submission

---
+++ CoderByte/TestFramework.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CoderByte
+{
+    class TestFramework
+    {
+        private IWebDriver Driver;
+
+        public TestFramework(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public int hrefAmountOnPage(string url, string href)
+        {
+            Driver.Navigate().GoToUrl(url);
+            return Driver.FindElements(By.XPath($"//a[@href='{href}']")).Count;
+        }
+
+        public (string Firstname, string Lastname) CheckReqresUserNamesByID(int id)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://reqres.in/api/users/{id}");
+                request.Method = "HEAD";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            }
+            finally
+            {
+                Driver.Navigate().GoToUrl($"https://reqres.in/api/users/{id}");
+            }
+            JObject user = JObject.Parse(Driver.FindElement(By.TagName("pre")).Text);
+            string first_name = (string)user["data"]["first_name"];
+            string last_name = (string)user["data"]["last_name"];
+
+            return (first_name, last_name);
+
+        }
+
+
+        public void Teardown()
+        {
+            Driver.Quit();
+        }
+    }
+}
--- CoderByte/UnitTest1.cs
+++ CoderByte/UnitTest1.cs
@@ -1,32 +1,74 @@
 using NUnit.Framework;
 using System;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.IO;
+using System.Net;
 
 namespace CoderByte
 {
     public class Tests
     {
-        IWebDriver driver;
-        string Path;
-        [SetUp]
+        TestFramework tf;
+        //IWebDriver driver;
+
+        [OneTimeSetUp]
         public void Setup()
         {
-            Path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            driver = new ChromeDriver(Path+@"\Drivers\");
+            string Path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            tf = new TestFramework(new ChromeDriver(Path + @"\Drivers\"));
         }
 
         [Test]
-        public void Test1()
+        public void TestFaceBookURL()
         {
-            driver.Navigate().GoToUrl("http://www.crawco.co.uk");
+            Assert.AreNotEqual(tf.hrefAmountOnPage("http://www.crawco.co.uk", "https://www.facebook.com/crawfordandco"), 0);
+        }
+
+        
+        [Test]
+        public void TestApiUser4()
+        {
+            var user = tf.CheckReqresUserNamesByID(4);
+            Assert.Multiple(() =>
+            {
+                Assert.That(user.Firstname, Is.EqualTo("Eve"));
+                Assert.That(user.Lastname, Is.EqualTo("Holt"));
+            });
+        }
+
+        [Test]
+        public void TestApiUser6()
+        {
+            var user = tf.CheckReqresUserNamesByID(6);
+            Assert.Multiple(() =>
+            {
+                Assert.That(user.Firstname, Is.EqualTo("Sergio"));
+                Assert.That(user.Lastname, Is.EqualTo("Ramos"));
+            });
+            //This Test is designed to fail as Firstname is Tracey, not Sergio
+        }
+
+        [Test]
+        public void TestApiUser23()
+        {
+            /*
+             https://www.selenium.dev/documentation/worst_practices/http_response_codes/
+             Selenium doesn't include a simple way to check a pages error codes unless they're represented in page
+             text, header or title, so plain C# will be used to check the page for a protocol error code, 
+             then it will be visited by selenium
+            */
+            Assert.Multiple(() =>
+            {
+               WebException ex = Assert.Throws<WebException>(() => { tf.CheckReqresUserNamesByID(23); });
+                Assert.That(ex.Status, Is.EqualTo(WebExceptionStatus.ProtocolError));
+            });
+
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
-            driver.Quit();
+            tf.Teardown();
         }
     }
 }
